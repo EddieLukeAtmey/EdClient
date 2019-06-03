1256c6e499883ae9a53cc5b4e42f40b344e454e8
Eddie <eddie.marvin116@gmail.com>
2019-06-04 01:25:59 +0700

    Initial commit

---
+++ EdClient/EdClient.h
@@ -0,0 +1,19 @@
+//
+//  EdClient.h
+//  EdClient
+//
+//  Created by Eddie Luke Atmey on 06/04/19.
+//  Copyright © 2019 Eddie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EdClient.
+FOUNDATION_EXPORT double EdClientVersionNumber;
+
+//! Project version string for EdClient.
+FOUNDATION_EXPORT const unsigned char EdClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EdClient/PublicHeader.h>
+
+
